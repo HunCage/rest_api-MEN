48d5ecff8ce1d58efbaa3629c394fac77ad4f685
HunCage <gkipeter@gmail.com>
2021-05-02 19:19:14 +0200

    models been added

--- routes/json.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    const data = "Hello JSON";
-    res.send({ "message": data });
-});
-
-module.exports = router;
