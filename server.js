25b684b07a53293dbe1987846fdfdbb36633dcfb
HunCage <gkipeter@gmail.com>
2021-05-02 19:05:31 +0200

    DB is initializiert

--- server.js
+++ server.js
@@ -1,54 +1,45 @@
-const express = require('express');
-const cors = require('cors');
-const { static } = require('express');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/db");
 
 const app = express();
+const db = process.env.DATABASE_URI;
 const PORT = process.env.PORT || 3000;
+
+// Connect Database
+connectDB(db);
 
 app.use(cors());
 
-app.options('*', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Authorization, Content-Length, X-Requested-With');
+app.options("*", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-Requested-With");
     res.send(200);
 });
 
 app.use(express.json());
-app.use(express.static('public'));
-app.use(express.static('css'));
-app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`);
     next();
 });
 
-const timeRoutes = require("./routes/time");
-const nameRoutes = require('./routes/name');
-const jsonRoutes = require("./routes/json");
-const echoAllRoutes = require("./routes/echo-all");
-
-app.use('/routes/time', timeRoutes);
-app.use('/routes/name', nameRoutes);
-app.use("/routes/json", jsonRoutes);
-app.use("/routes/echo-all", echoAllRoutes);
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
+/* app.use("/api/quotes", require("./routes/api/quotes"));
+app.use("/api/authors", require("./routes/api/authors"));
+app.use("/api/categories", require("./routes/api/categories"));
+ */
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
 
-app.get('/form', (req, res) => {
-    res.sendFile(__dirname + "/views/form.html");
-});
-
-app.get('/:word/echo', (req, res) => {
-    res.json({ "echo": req.params.word });
-});
-
-app.all('*', (req, res) => {
+app.all("*", (req, res) => {
     res.send("Invalid route");
 });
 
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+// app.use(express.static('public'));
+// app.use(express.static('css'));
+// app.use(express.urlencoded({ extended: false }));
 
-app.listen(PORT, console.log(`Server is listening on port: ${PORT}`));
